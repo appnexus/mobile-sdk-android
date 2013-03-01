b0ae3276f0960b4235687693a8f0a99e9ac5a978
Jacob Shufro <jshufro@appnexus.com>
2013-03-01 15:52:15 -0500

    Sample app changes for MS-78

--- examples/helloworld/src/com/example/helloworld/MainActivity.java
+++ examples/helloworld/src/com/example/helloworld/MainActivity.java
@@ -1,23 +1,32 @@
 package com.example.helloworld;
 
 import com.appnexus.mraid.MRAIDWebView;
+import com.appnexus.mraid.MRAIDTests;
 import com.appnexus.opensdk.BannerAdView;
 import com.appnexus.opensdk.InterstitialAdView;
 import com.appnexus.opensdk.AdListener;
+import com.google.ads.AdRequest;
+import com.google.ads.AdSize;
+import com.google.ads.AdView;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.gesture.GestureOverlayView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity implements AdListener{
 	private BannerAdView av;
 	InterstitialAdView iav;
+	RelativeLayout layout;
 	private int interstitials;
 	Button showButton;
+	private AdView adview;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +43,26 @@ public class MainActivity extends Activity implements AdListener{
 		
 		//mraid
 		MRAIDWebView mraid_space = (MRAIDWebView) findViewById(R.id.mraid_container);
-		mraid_space.load(mraid_space.getAdMobTestBannerDotJs());
+		mraid_space.load(MRAIDTests.getAdMobTestJs(this.getResources()));
+		
+		//admob
+		if(true) return;
+		layout=(RelativeLayout) findViewById(R.id.mainview);
+		AdView av = new AdView(this, AdSize.BANNER, "a1512e787095f19");
+		RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, 50);
+		lp.addRule(RelativeLayout.ABOVE, R.id.ll);
+		lp.alignWithParent=true;
+		av.setLayoutParams(lp);
+		
+		layout.addView(av);
+		AdRequest ar = new AdRequest();
+		ar.addTestDevice("AE736B8A6A42CDC5B796A8A2BAB34524");
+		av.loadAd(ar);
+		GestureOverlayView gov = (GestureOverlayView)av.getChildAt(0);
+		FrameLayout fl = (FrameLayout) gov.getChildAt(0);
+		for(int i=0;i<fl.getChildCount();i++){
+			Log.d("ADMOB", fl.getChildAt(i).toString());
+		}
 	}
 
 	@Override
