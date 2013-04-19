42d33ac39abd3d5c03087cdee3649ecfaedf8c85
Jacob Shufro <jshufro@appnexus.com>
2013-04-19 11:30:21 -0400

    Conversion pixels, working

--- sdk/src/com/appnexus/opensdk/InstallTrackerPixel.java
+++ sdk/src/com/appnexus/opensdk/InstallTrackerPixel.java
@@ -1,17 +1,35 @@
 package com.appnexus.opensdk;
 
+import java.util.ArrayList;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import com.appnexus.opensdk.utils.Clog;
+import com.appnexus.opensdk.utils.Settings;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class InstallTrackerPixel extends BroadcastReceiver{
 	BroadcastReceiver receiver_install;
+	Context context;
+	
+	
+	//Test with am broadcast -a com.android.vending.INSTALL_REFERRER --es "referrer" "utm_source=test_source&utm_medium=test_medium&utm_term=test_term&utm_content=test_content&utm_campaign=test_name"
+	//in adb
+	public InstallTrackerPixel(){
+		super();
+	}
 
 	@Override
 	public void onReceive(Context context, final Intent intent) {
+		this.context=context;
 		new Thread(new Runnable(){
 
 			@Override
@@ -19,11 +37,69 @@ public class InstallTrackerPixel extends BroadcastReceiver{
 				Bundle extras = intent.getExtras();
 				String referralString = extras.getString("referrer");
 
-				Clog.e("OPENSDK-DEBUG", referralString);
+				ArrayList<BasicNameValuePair> parameters = getParameters(referralString);
+				String url = getPixelUrl(parameters);
+				
+				Clog.d(Clog.baseLogTag, "Firing pixel: "+url);
+				
+				try{
+					HttpClient client = new DefaultHttpClient();
+					HttpGet get = new HttpGet(url);
+					client.execute(get);
+					// TODO: what happens if it fails? what is the server response if it succeeds?
+				}catch(Exception e){
+					e.printStackTrace();
+					// TODO: repeat the request later?
+				}
 				
 			}
 		}).run();
 
+	}
+	
+	private ArrayList<BasicNameValuePair> getParameters(String s){
+		ArrayList<BasicNameValuePair> output = new ArrayList<BasicNameValuePair>();
+		String[] fields = s.split("&");
+		for(String pair : fields){
+			output.add(new BasicNameValuePair(pair.split("=")[0], pair.split("=")[1]));
+		}
+		return output;
+	}
+	
+	private String getPixelUrl(ArrayList<BasicNameValuePair> input){
+		String source = null;
+		String medium = null;
+		String term = null;
+		String content = null;
+		String campaign = null;
+		String appid = null;
+		
+		for(BasicNameValuePair p : input){
+			if(p.getName().equals("utm_source")){
+				source=p.getValue();
+			}else if(p.getName().equals("utm_medium")){
+				medium=p.getValue();
+			}else if(p.getName().equals("utm_term")){
+				term=p.getValue();
+			}else if(p.getName().equals("utm_content")){
+				content=p.getValue();
+			}else if(p.getName().equals("utm_campaign")){
+				campaign=p.getValue();
+			}
+		}
+		
+		if(context!=null) appid = context.getApplicationContext()
+				.getPackageName();
+		
+		StringBuilder urlBuilder = new StringBuilder(Settings.getSettings().PIXEL_BASE_URL);
+		urlBuilder.append(source!=null?"source="+Uri.encode(source):"");
+		urlBuilder.append(medium!=null?"&medium="+Uri.encode(medium):"");
+		urlBuilder.append(term!=null?"&term="+Uri.encode(term):"");
+		urlBuilder.append(content!=null?"&content="+Uri.encode(content):"");
+		urlBuilder.append(campaign!=null?"&campaign="+Uri.encode(campaign):"");
+		urlBuilder.append(appid!=null?"&appid="+Uri.encode(appid):"");
+		
+		return urlBuilder.toString();
 	}
 
 }
