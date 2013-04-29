f560d7384eb7266eaf1a831bab589d9a6847263f
Jacob Shufro <jshufro@appnexus.com>
2013-04-29 16:00:05 -0400

    Dummy views all done, ready to add logic

--- examples/demo/src/com/example/opensdkdemo/DebugTabFragment.java
+++ examples/demo/src/com/example/opensdkdemo/DebugTabFragment.java
@@ -1,11 +1,19 @@
 package com.example.opensdkdemo;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class DebugTabFragment extends Fragment {
 
-	public DebugTabFragment() {
-		// TODO Auto-generated constructor stub
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+		View out = inflater.inflate(R.layout.fragment_debug, null);
+		
+		//onCreate stuff here
+		
+		return out;
 	}
-
 }
