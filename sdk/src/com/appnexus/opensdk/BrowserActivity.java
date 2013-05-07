12dc7d0813b9cd887ae6db1d0bcbb7c83a0921d1
Jacob Shufro <jshufro@appnexus.com>
2013-05-07 14:50:41 -0400

    Enable JS, set up buttons, and copy behavior for MRAID web view

--- sdk/src/com/appnexus/opensdk/BrowserActivity.java
+++ sdk/src/com/appnexus/opensdk/BrowserActivity.java
@@ -1,14 +1,79 @@
 package com.appnexus.opensdk;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.webkit.WebSettings.PluginState;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageButton;
 
 
 public class BrowserActivity extends Activity {
+	private WebView webview;
+	private ImageButton back;
+	private ImageButton forward;
+	private ImageButton refresh;
 
+	@SuppressLint("SetJavaScriptEnabled")
 	@Override
 	public void onCreate(Bundle savedInstance){
 		super.onCreate(savedInstance);
 		setContentView(R.layout.activity_in_app_browser);
 		
+		webview = (WebView) findViewById(R.id.web_view);
+		back = (ImageButton) findViewById(R.id.browser_back);
+		forward = (ImageButton) findViewById(R.id.browser_forward);
+		refresh = (ImageButton) findViewById(R.id.browser_refresh);
+		
+		webview.getSettings().setJavaScriptEnabled(true);
+		webview.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+		webview.getSettings().setDomStorageEnabled(true);
+		webview.getSettings().setPluginState(PluginState.ON_DEMAND);
+		
+		
+		back.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				if(webview.canGoBack()){
+					webview.goBack();
+				}else{
+					finish();
+				}
+			}
+			
+		});
+		
+		forward.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v){
+				webview.goForward();
+			}
+		});
+		
+		refresh.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v){
+				webview.reload();
+			}
+		});
+		
+		
+		webview.setWebViewClient(new WebViewClient(){
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url){
+				if(url.startsWith("http")){
+					webview.loadUrl(url);
+					return true;
+				}
+				return false;
+			}
+		});
+		
+		String url = (String) getIntent().getExtras().get("url");
+		
+		webview.loadUrl(url);
 	}
 }
--- sdk/src/com/appnexus/opensdk/MRAIDImplementation.java
+++ sdk/src/com/appnexus/opensdk/MRAIDImplementation.java
@@ -28,32 +28,35 @@ import android.webkit.WebViewClient;
 @SuppressLint("InlinedApi")
 public class MRAIDImplementation {
 	MRAIDWebView owner;
-	boolean readyFired=false;
-	boolean expanded=false;
-	boolean hidden=false;
+	boolean readyFired = false;
+	boolean expanded = false;
+	boolean hidden = false;
 	int default_width, default_height;
-	
-	public MRAIDImplementation(MRAIDWebView owner){
-		this.owner=owner;
+
+	public MRAIDImplementation(MRAIDWebView owner) {
+		this.owner = owner;
 	}
-	
-	//The webview about to load the ad, and the html ad content
-	protected String onPreLoadContent(WebView wv, String html){
-		//Check to ensure <html> tags are present
-		if(!html.contains("<html>")){
-			html="<html><head></head><body style='padding:0;margin:0;'>"+html+"</body></html>";
+
+	// The webview about to load the ad, and the html ad content
+	protected String onPreLoadContent(WebView wv, String html) {
+		// Check to ensure <html> tags are present
+		if (!html.contains("<html>")) {
+			html = "<html><head></head><body style='padding:0;margin:0;'>"
+					+ html + "</body></html>";
 		} else if (!html.contains("<head>")) {
 			// The <html> tags are present, but there is no <head> section to
 			// inject the mraid js
 			html = html.replace("<html>", "<html><head></head>");
 		}
-		
-		//Insert mraid script source
-		html=html.replace("<head>", "<head><script>"+getMraidDotJS(wv.getResources())+"</script>");
-		
+
+		// Insert mraid script source
+		html = html.replace("<head>",
+				"<head><script>" + getMraidDotJS(wv.getResources())
+						+ "</script>");
+
 		return html;
 	}
-	
+
 	protected String getMraidDotJS(Resources r) {
 		InputStream ins = r.openRawResource(R.raw.mraid);
 		try {
@@ -62,65 +65,78 @@ public class MRAIDImplementation {
 			return new String(buffer, "UTF-8");
 		} catch (IOException e) {
 			return null;
-		}	
+		}
 	}
-	
+
 	protected void onReceivedError(WebView view, int errorCode, String desc,
 			String failingUrl) {
-		Clog.w(Clog.mraidLogTab, Clog.getString(R.string.webview_received_error, errorCode, desc, failingUrl));
+		Clog.w(Clog.mraidLogTab, Clog.getString(
+				R.string.webview_received_error, errorCode, desc, failingUrl));
 	}
-	
-	protected WebViewClient getWebViewClient() {
-		
 
-		return new WebViewClient(){
+	protected WebViewClient getWebViewClient() {
+
+		return new WebViewClient() {
 
 			@Override
 			public boolean shouldOverrideUrlLoading(WebView view, String url) {
 				if (!url.startsWith("mraid:") && !url.startsWith("javascript:")) {
-					Intent intent = new Intent(Intent.ACTION_VIEW,
-							Uri.parse(url));
-					view.getContext().startActivity(intent);
+					if (owner.owner.getOpensNativeBrowser()) {
+						Intent intent = new Intent(Intent.ACTION_VIEW,
+								Uri.parse(url));
+						owner.getContext().startActivity(intent);
+					}else{
+						Intent intent = new Intent(owner.getContext(), BrowserActivity.class);
+						intent.putExtra("url", url);
+						owner.getContext().startActivity(intent);
+					}
 					return true;
 				}
-				if(url.startsWith("mraid://")){
+				if (url.startsWith("mraid://")) {
 					MRAIDImplementation.this.dispatch_mraid_call(url);
-					
+
 					return true;
 				}
-				
+
 				// See if any native activities can handle the Url
-				try{
-					owner.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+				try {
+					owner.getContext().startActivity(
+							new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
 					return true;
-				} catch(ActivityNotFoundException e){
+				} catch (ActivityNotFoundException e) {
 					return false;
 				}
 			}
-			
+
 			@Override
-			public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error){
-				Clog.e(Clog.httpRespLogTag, Clog.getString(R.string.webclient_error, error.getPrimaryError(), error.toString()));
+			public void onReceivedSslError(WebView view,
+					SslErrorHandler handler, SslError error) {
+				Clog.e(Clog.httpRespLogTag,
+						Clog.getString(R.string.webclient_error,
+								error.getPrimaryError(), error.toString()));
 			}
-			
+
 			@Override
-			public void onReceivedError(WebView view, int errorCode, String description, String failingURL){
-				Clog.e(Clog.httpRespLogTag, Clog.getString(R.string.webclient_error, errorCode, description));
+			public void onReceivedError(WebView view, int errorCode,
+					String description, String failingURL) {
+				Clog.e(Clog.httpRespLogTag, Clog.getString(
+						R.string.webclient_error, errorCode, description));
 			}
-			
+
 			@Override
 			public void onPageFinished(WebView view, String url) {
-				//Fire the ready event only once
-				if(!readyFired){
-					view.loadUrl("javascript:window.mraid.util.setPlacementType('"+owner.owner.getMRAIDAdType()+"')");
+				// Fire the ready event only once
+				if (!readyFired) {
+					view.loadUrl("javascript:window.mraid.util.setPlacementType('"
+							+ owner.owner.getMRAIDAdType() + "')");
 					view.loadUrl("javascript:window.mraid.util.setIsViewable(true)");
 					view.loadUrl("javascript:window.mraid.util.stateChangeEvent('default')");
 					view.loadUrl("javascript:window.mraid.util.readyEvent();");
-					
-					//Store width and height for close()
+
+					// Store width and height for close()
 					default_width = owner.getLayoutParams().width;
 					default_height = owner.getLayoutParams().height;
-					
+
 					readyFired = true;
 				}
 			}
@@ -128,18 +144,24 @@ public class MRAIDImplementation {
 	}
 
 	protected WebChromeClient getWebChromeClient() {
-		return new WebChromeClient(){
+		return new WebChromeClient() {
 			@Override
-			public boolean onConsoleMessage(ConsoleMessage consoleMessage){
-				//super.onConsoleMessage(consoleMessage);
-				Clog.w(Clog.mraidLogTab, Clog.getString(R.string.console_message, consoleMessage.message(), consoleMessage.lineNumber(),consoleMessage.sourceId()));
+			public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+				// super.onConsoleMessage(consoleMessage);
+				Clog.w(Clog.mraidLogTab,
+						Clog.getString(R.string.console_message,
+								consoleMessage.message(),
+								consoleMessage.lineNumber(),
+								consoleMessage.sourceId()));
 				return true;
 			}
-			
+
 			@Override
-			public boolean onJsAlert(WebView view, String url, String message, JsResult result){
-				///super.onJsAlert(view, url, message, result);
-				Clog.w(Clog.mraidLogTab, Clog.getString(R.string.js_alert, message, url));
+			public boolean onJsAlert(WebView view, String url, String message,
+					JsResult result) {
+				// /super.onJsAlert(view, url, message, result);
+				Clog.w(Clog.mraidLogTab,
+						Clog.getString(R.string.js_alert, message, url));
 				result.confirm();
 				return true;
 			}
@@ -147,81 +169,90 @@ public class MRAIDImplementation {
 	}
 
 	protected void onVisible() {
-		if(readyFired)
+		if (readyFired)
 			owner.loadUrl("javascript:window.mraid.util.setIsViewable(true)");
-		
+
 	}
 
 	protected void onInvisible() {
-		if(readyFired)
+		if (readyFired)
 			owner.loadUrl("javascript:window.mraid.util.setIsViewable(false)");
 	}
-	
-	protected void close(){
-		if(expanded){
-			AdView.LayoutParams lp = new AdView.LayoutParams(owner.getLayoutParams());
-			lp.height=default_height;
-			lp.width=default_width;
-			lp.gravity=Gravity.CENTER;
+
+	protected void close() {
+		if (expanded) {
+			AdView.LayoutParams lp = new AdView.LayoutParams(
+					owner.getLayoutParams());
+			lp.height = default_height;
+			lp.width = default_width;
+			lp.gravity = Gravity.CENTER;
 			owner.setLayoutParams(lp);
 			owner.close();
-			this.owner.loadUrl("javascript:window.mraid.util.stateChangeEvent('default');");
-			
-			//Allow orientation changes
-			Activity a = ((Activity)this.owner.getContext());
-			if(a!=null)
-				a.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
-			expanded=false;
-		}else{
-			//state must be default
-			owner.hide();
-			hidden=true;
-		}
-	}
-	
-	protected void expand(ArrayList<BasicNameValuePair> parameters) {
-		if(!hidden){
-			int width=owner.getLayoutParams().width;//Use current height and width as expansion defaults.
-			int height=owner.getLayoutParams().height;
-			boolean useCustomClose=false;
-			for(BasicNameValuePair bnvp : parameters){
-				if(bnvp.getName().equals("w")) width = Integer.parseInt(bnvp.getValue());
-				else if(bnvp.getName().equals("h")) height = Integer.parseInt(bnvp.getValue());
-				else if(bnvp.getName().equals("useCustomClose")) useCustomClose = Boolean.parseBoolean(bnvp.getValue());
-			}
-		
-			owner.expand(width, height, useCustomClose, this);
-			//Fire the stateChange
-			this.owner.loadUrl("javascript:window.mraid.util.stateChangeEvent('expanded');");
-			expanded=true;
-			//Lock the orientation
-			AdActivity.lockOrientation((Activity)this.owner.getContext());
+			this.owner
+					.loadUrl("javascript:window.mraid.util.stateChangeEvent('default');");
 
-		}else{
-			owner.show();
-			hidden=false;
+			// Allow orientation changes
+			Activity a = ((Activity) this.owner.getContext());
+			if (a != null)
+				a.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+			expanded = false;
+		} else {
+			// state must be default
+			owner.hide();
+			hidden = true;
 		}
 	}
-	
+
+	protected void expand(ArrayList<BasicNameValuePair> parameters) {
+		if (!hidden) {
+			int width = owner.getLayoutParams().width;// Use current height and
+														// width as expansion
+														// defaults.
+			int height = owner.getLayoutParams().height;
+			boolean useCustomClose = false;
+			for (BasicNameValuePair bnvp : parameters) {
+				if (bnvp.getName().equals("w"))
+					width = Integer.parseInt(bnvp.getValue());
+				else if (bnvp.getName().equals("h"))
+					height = Integer.parseInt(bnvp.getValue());
+				else if (bnvp.getName().equals("useCustomClose"))
+					useCustomClose = Boolean.parseBoolean(bnvp.getValue());
+			}
+
+			owner.expand(width, height, useCustomClose, this);
+			// Fire the stateChange
+			this.owner
+					.loadUrl("javascript:window.mraid.util.stateChangeEvent('expanded');");
+			expanded = true;
+			// Lock the orientation
+			AdActivity.lockOrientation((Activity) this.owner.getContext());
+
+		} else {
+			owner.show();
+			hidden = false;
+		}
+	}
+
 	protected void dispatch_mraid_call(String url) {
-		//Remove the fake protocol
+		// Remove the fake protocol
 		url = url.replaceFirst("mraid://", "");
-		
-		//Separate the function from the parameters
+
+		// Separate the function from the parameters
 		String func = url.split("\\?")[0].replaceAll("/", "");
 		String params;
 		ArrayList<BasicNameValuePair> parameters = new ArrayList<BasicNameValuePair>();
-		if(url.split("\\?").length>1){
+		if (url.split("\\?").length > 1) {
 			params = url.split("\\?")[1];
-		
-			for(String s : params.split("&")){
-				parameters.add(new BasicNameValuePair(s.split("=")[0], s.split("=")[1]));
+
+			for (String s : params.split("&")) {
+				parameters.add(new BasicNameValuePair(s.split("=")[0], s
+						.split("=")[1]));
 			}
 		}
-		
-		if(func.equals("expand")){
+
+		if (func.equals("expand")) {
 			expand(parameters);
-		}else if(func.equals("close")){
+		} else if (func.equals("close")) {
 			close();
 		}
 	}
