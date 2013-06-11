34f997547fdd6ff61969c95a990a48e1a9b11b6c
Jacob Shufro <jshufro@appnexus.com>
2013-06-11 16:53:44 -0400

    Added license and license headers

--- sdk/res/raw/test.js
+++ sdk/res/raw/test.js
@@ -1,4 +1,20 @@
-    <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+    
+<!--
+   Copyright 2013 APPNEXUS INC
+   
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+-->
+<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
     <meta name='viewport' content='user-scalable=no, width=device-width,initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'/>
     <script type='text/javascript' src='mraid.js'></script>
     <script type='text/javascript'>
