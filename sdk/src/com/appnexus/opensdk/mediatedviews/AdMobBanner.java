b9d29d6d384c941855becefc829237d14a482801
Jacob Shufro <jshufro@appnexus.com>
2013-07-29 14:00:07 -0400

    Add first stock mediated view

---
+++ sdk/src/com/appnexus/opensdk/mediatedviews/AdMobBanner.java
@@ -0,0 +1,21 @@
+package com.appnexus.opensdk.mediatedviews;
+
+import android.app.Activity;
+import android.view.View;
+
+import com.appnexus.opensdk.MediatedBannerAdView;
+
+public class AdMobBanner implements MediatedBannerAdView {
+
+	public AdMobBanner() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public View requestAd(Activity activity, String parameter, String uid,
+			int width, int height, View adSpace) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
