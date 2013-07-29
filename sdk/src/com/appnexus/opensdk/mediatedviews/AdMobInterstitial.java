6dfc7efa04f1e148b1f5d5c22bfea1f8d0e354d5
Jacob Shufro <jshufro@appnexus.com>
2013-07-29 16:43:14 -0400

    Add interstitial class

---
+++ sdk/src/com/appnexus/opensdk/mediatedviews/AdMobInterstitial.java
@@ -0,0 +1,21 @@
+package com.appnexus.opensdk.mediatedviews;
+
+import android.app.Activity;
+import android.view.View;
+
+import com.appnexus.opensdk.MediatedInterstitialAdView;
+
+public class AdMobInterstitial implements MediatedInterstitialAdView {
+
+	public AdMobInterstitial() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public View requestAd(Activity activity, String parameter, String uid,
+			int width, int height) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
