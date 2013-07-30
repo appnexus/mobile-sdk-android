edef9b024a2560addbcb000ce8e03defb7512a21
Jacob Shufro <jshufro@appnexus.com>
2013-07-30 14:32:18 -0400

    Code to display admob mediated banners done

--- sdk/src/com/appnexus/opensdk/mediatedviews/AdMobBanner.java
+++ sdk/src/com/appnexus/opensdk/mediatedviews/AdMobBanner.java
@@ -1,21 +1,30 @@
 package com.appnexus.opensdk.mediatedviews;
 
 import android.app.Activity;
+import android.content.Context;
+import android.telephony.TelephonyManager;
 import android.view.View;
+
+import com.google.ads.*;
 
 import com.appnexus.opensdk.MediatedBannerAdView;
 
 public class AdMobBanner implements MediatedBannerAdView {
-
+	
 	public AdMobBanner() {
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public View requestAd(Activity activity, String parameter, String uid,
 			int width, int height, View adSpace) {
-		// TODO Auto-generated method stub
-		return null;
+		AdView admobAV = new AdView(activity, new AdSize(width, height), uid);
+		AdRequest ar = new AdRequest();
+		
+		//TODO remove
+		ar.addTestDevice(((TelephonyManager)activity.getBaseContext().getSystemService(Context.TELEPHONY_SERVICE)).getDeviceId());
+	
+		admobAV.loadAd(ar);
+		return admobAV;
 	}
 
 }
