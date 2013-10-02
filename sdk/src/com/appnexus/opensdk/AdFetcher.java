2592f205b2f83e76422c7451c436f723e7dfdfa2
Mark Ha <mha@appnexus.com>
2013-10-02 11:35:29 -0400

    MS-249 re-add callback since this is before callbacks were refactored. delete later in merge

--- sdk/src/com/appnexus/opensdk/AdFetcher.java
+++ sdk/src/com/appnexus/opensdk/AdFetcher.java
@@ -264,6 +264,8 @@ public class AdFetcher implements AdRequester {
     @Override
     public void onReceiveResponse(AdResponse response) {
         dispatchResponse(response);
+        // until callbacks branch is merged, leave this here
+        owner.onAdLoaded();
     }
 
 	@Override
