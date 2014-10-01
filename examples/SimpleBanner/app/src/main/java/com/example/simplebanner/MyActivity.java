2ac32af3c0554ae207e61f4c37ffbebb81b31196
Jacob Shufro <jshufro@appnexus.com>
2014-10-01 17:17:56 -0400

    MS-707 add a simple example

---
+++ examples/SimpleBanner/app/src/main/java/com/example/simplebanner/MyActivity.java
@@ -0,0 +1,112 @@
+/*
+ *    Copyright 2014 APPNEXUS INC
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.example.simplebanner;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.FrameLayout;
+import android.location.LocationManager;
+import android.location.Location;
+import android.os.Handler;
+import com.appnexus.opensdk.*;
+import com.appnexus.opensdk.utils.Clog;
+
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        final BannerAdView bav = new BannerAdView(this);
+
+	// This is your AppNexus placement ID.
+        bav.setPlacementID("1326299");
+	
+	// Turning this on so we always get an ad during testing.
+        bav.setShouldServePSAs(true);
+
+	// By default ad clicks open in an in-app WebView.
+        bav.setOpensNativeBrowser(true);
+
+	// Get a 300x50 ad.
+        bav.setAdSize(300, 50);
+
+	// Set up a listener on this ad view that logs events.
+	AdListener adListener = new AdListener() {
+		@Override
+		public void onAdRequestFailed(AdView bav, ResultCode errorCode) {
+		    if (errorCode == null) {
+			Clog.v("SIMPLEBANNER", "Call to loadAd failed");
+		    } else {
+			Clog.v("SIMPLEBANNER", "Ad request failed: " + errorCode);
+		    }
+		}
+
+		@Override
+		public void onAdLoaded(AdView bav) {
+		    Clog.v("SIMPLEBANNER", "The Ad Loaded!");
+		}
+
+		@Override
+		public void onAdExpanded(AdView bav) {
+		    Clog.v("SIMPLEBANNER", "Ad expanded");
+		}
+
+		@Override
+		public void onAdCollapsed(AdView bav) {
+		    Clog.v("SIMPLEBANNER", "Ad collapsed");
+		}
+
+		@Override
+		public void onAdClicked(AdView bav) {
+		    Clog.v("SIMPLEBANNER", "Ad clicked; opening browser");
+		}
+	    };
+
+	bav.setAdListener(adListener);
+
+	// Get the device's location and send it on the ad call so the
+	// impression is more attractive to advertisers.
+	LocationManager locationManager =
+	    (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+
+	Location location =
+	    locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+	SDKSettings.setLocation(location);
+
+        FrameLayout layout = (FrameLayout)findViewById(android.R.id.content);
+        layout.addView(bav);
+
+	// If auto-refresh is enabled (the default), a call to
+	// `FrameLayout.addView()` followed directly by
+	// `BannerAdView.loadAd()` will succeed.  However, if
+	// auto-refresh is disabled, the call to
+	// `BannerAdView.loadAd()` needs to be wrapped in a `Handler`
+	// block to ensure that the banner ad view is in the view
+	// hierarchy *before* the call to `loadAd()`.  Otherwise the
+	// visibility check in `loadAd()` will fail, and no ad will be
+	// shown.
+	new Handler().postDelayed(new Runnable() {
+		@Override
+		public void run() {
+		    bav.loadAd();
+		}
+	    }, 0);
+    }
+}
