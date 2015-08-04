d86b930a2382c17392cb21c5137f3ed43038cec4
Wei Zhang <wzhang@appnexus.com>
2015-08-04 11:53:44 -0400

    update mediated network versions

--- sdk/src/com/appnexus/opensdk/utils/Settings.java
+++ sdk/src/com/appnexus/opensdk/utils/Settings.java
@@ -42,7 +42,7 @@ public class Settings {
     public boolean test_mode = false;
     public String ua = null;
     public boolean first_launch;
-    public final String sdkVersion = "2.3.1";
+    public final String sdkVersion = "2.4";
 
     public String mcc;
     public String mnc;
