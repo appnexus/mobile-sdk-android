f0ce77d5e5b458073abff41985f8fd8c34e7beff
rpagaria <ramit.pagaria@synechron.com>
2015-11-10 15:23:36 -0500

    Universal tag - V2 Initial commit
    1. HTML Response parsed
    2. VAST response parsed
    3. Notify URL added to impressions list for VAST
    4. New methods created for V2. Old kept for reference for now.

--- sdk/src/com/appnexus/opensdk/UTAdResponse.java
+++ sdk/src/com/appnexus/opensdk/UTAdResponse.java
@@ -44,6 +44,11 @@ class UTAdResponse {
     private static final String RESPONSE_KEY_HEIGHT = "height";
     public static final String RESPONSE_KEY_NO_BID = "nobid";
 
+    //UT-V2
+    private static final String RESPONSE_KEY_RTB = "rtb";
+    private static final String RESPONSE_KEY_ADS = "ads";
+    private static final String RESPONSE_KEY_NOTIFY_URL = "notify_url";
+
     private AdModel vastAdResponse;
     private String content;
     private int height;
@@ -69,7 +74,7 @@ class UTAdResponse {
         Clog.i(Clog.httpRespLogTag, "Media type: "+mediaType);
 
         this.mediaType = mediaType;
-        parseResponse(body);
+        parseResponseV2(body);
     }
 
 
@@ -121,6 +126,51 @@ class UTAdResponse {
         }
     }
 
+    private void parseResponseV2(String body) {
+        JSONObject response;
+
+        try {
+            if (!StringUtil.isEmpty(body)) {
+                response = new JSONObject(body);
+            } else {
+                Clog.e(Clog.httpRespLogTag,  "NO Response: "+ body);
+                return;
+            }
+        } catch (JSONException e) {
+            Clog.e(Clog.httpRespLogTag,  Clog.getString(R.string.response_json_error, body));
+            return;
+        }
+
+        try {
+            JSONArray tagsArray = JsonUtil.getJSONArray(response, RESPONSE_KEY_TAGS);
+            if(tagsArray != null) {
+                JSONObject tagObject = JsonUtil.getJSONObjectFromArray(tagsArray, 0);
+                // If it contains nobid response, don't parse further.
+                if (JsonUtil.getJSONBoolean(tagObject, RESPONSE_KEY_NO_BID)){
+                    return;
+                }
+
+                JSONArray adArray = JsonUtil.getJSONArray(tagObject, RESPONSE_KEY_ADS);
+                JSONObject adObject = JsonUtil.getJSONObjectFromArray(adArray, 0);
+
+                JSONObject rtbObject = JsonUtil.getJSONObject(adObject, RESPONSE_KEY_RTB);
+                if (rtbObject != null) {
+                    if(rtbObject.has(RESPONSE_KEY_BANNER)) {
+                        Clog.i(Clog.httpReqLogTag, "it's an HTML Ad");
+                        parseHTMLAdV2(rtbObject);
+                    }else{
+                        Clog.i(Clog.httpReqLogTag, "it's a Video Ad");
+                        parseVastVideoAdV2(rtbObject, adObject.getString(RESPONSE_KEY_NOTIFY_URL));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            // Catches XMLPullParserException, JSONException, NullPointerException and IOException
+            Clog.e(Clog.httpReqLogTag, "Error parsing the ad response: " + e.getMessage());
+            containsAds = false;
+        }
+    }
+
     /**
      * Parses HTML ad response
      * @param adObject
@@ -144,6 +194,7 @@ class UTAdResponse {
         }
     }
 
+
     /**
      * Parses VAST ad response
      * @param adObject
@@ -166,6 +217,57 @@ class UTAdResponse {
             }
         }
     }
+
+
+    /**
+     * Parse UT-V2 HTML response
+     * @param rtbObject
+     * @throws Exception
+     */
+    private void parseHTMLAdV2(JSONObject rtbObject) throws Exception{
+        JSONObject bannerObject = JsonUtil.getJSONObject(rtbObject, RESPONSE_KEY_BANNER);
+        if(bannerObject != null) {
+            height = JsonUtil.getJSONInt(bannerObject, RESPONSE_KEY_HEIGHT);
+            width = JsonUtil.getJSONInt(bannerObject, RESPONSE_KEY_WIDTH);
+            content = JsonUtil.getJSONString(bannerObject, RESPONSE_KEY_CONTENT);
+            if (StringUtil.isEmpty(content)) {
+                Clog.e(Clog.httpRespLogTag, Clog.getString(R.string.blank_ad));
+            } else {
+                if (content.contains(ServerResponse.MRAID_JS_FILENAME)) {
+                    addToExtras(ServerResponse.EXTRAS_KEY_MRAID, true);
+                }
+                Clog.i(Clog.httpReqLogTag, "parseHTMLAd: true");
+                containsAds = true;
+            }
+        }
+    }
+
+    /**
+     *  Parse UT-V2 VAST response
+     * @param rtbObject
+     * @throws Exception
+     */
+    private void parseVastVideoAdV2(JSONObject rtbObject, String impressionUrl) throws Exception {
+
+        JSONObject videoObject = JsonUtil.getJSONObject(rtbObject, RESPONSE_KEY_VIDEO);
+        if(videoObject != null) {
+            String vastResponse = JsonUtil.getJSONString(videoObject, RESPONSE_KEY_CONTENT);
+            if(!StringUtil.isEmpty(vastResponse)) {
+                InputStream stream = new ByteArrayInputStream(vastResponse.getBytes(Charset.forName(UTF_8)));
+
+                VastResponseParser vastResponseParser = new VastResponseParser();
+                this.vastAdResponse = vastResponseParser.readVAST(stream);
+                if(this.vastAdResponse != null && this.vastAdResponse.containsLinearAd()) {
+                    containsAds = true;
+                    if(impressionUrl != null) {
+                        this.vastAdResponse.getImpressionArrayList().add(impressionUrl);
+                    }
+                    Clog.i(Clog.httpReqLogTag, "Vast response parsed");
+                }
+            }
+        }
+    }
+
 
 
 
--- sdk/src/com/appnexus/opensdk/utils/Settings.java
+++ sdk/src/com/appnexus/opensdk/utils/Settings.java
@@ -79,7 +79,9 @@ public class Settings {
     public static String COOKIE_DOMAIN = "http://mediation.adnxs.com";
     public static final String AN_UUID = "uuid2";
     public static String BASE_URL = "http://mediation.adnxs.com/";
-    public static final String BASE_URL_UT = "http://ib.adnxs.com/ut/v1";
+//    public static final String BASE_URL_UT = "http://rlissack.adnxs.net:8080/jtest/utv2html";
+    public static final String BASE_URL_UT = "http://rlissack.adnxs.net:8080/jtest/utv2video";
+//    public static final String BASE_URL_UT = "http://ib.adnxs.com/ut/v1";
     public static String REQUEST_BASE_URL = "http://mediation.adnxs.com/mob?";
     public static String INSTALL_BASE_URL = "http://mediation.adnxs.com/install?";
 
