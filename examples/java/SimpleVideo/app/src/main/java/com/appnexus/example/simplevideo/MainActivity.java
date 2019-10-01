afda6c3b6384af0617b7ba55b1a59230cc0f51cb
Abhas Vohra <avohra@appnexus.com>
2019-10-01 14:47:08 +0000

    Added Sample Kotlin App
    Squashed commit of the following:
    
    commit 4afad1dfdcbac2b472af816fc753603f388ad502
    Merge: e38bcd1 e5e9cb0
    Author: Abhas Vohra <avohra@appnexus.com>
    Date:   Tue Oct 1 15:47:16 2019 +0530
    
        Merge branch 'MS-3928_Sample_Kotlin_app' of ssh://stash.corp.appnexus.com:7999/mobile-sdk/app_mobile-sdk-android into MS-3928_Sample_Kotlin_app
    
    commit e38bcd1dd821674ef57f45b04dbe4c08454dbe69
    Author: Abhas Vohra <avohra@appnexus.com>
    Date:   Tue Oct 1 15:18:37 2019 +0530
    
        Updated the placement id for SimpleBanner and SimpleVideo
    
    commit e5e9cb0650acad58ea9f26bc415e7ff0abe71c27
    Author: Kowshickkarthick Subramanian <kowshickkarthick.subramanian@xandr.com>
    Date:   Mon Sep 30 14:31:10 2019 +0000
    
        update
    
        Squashed commit of the following:
    
        commit 6e2691f85e90158e3494b39963b67f8f3b094c54
        Author: ksubramanian <ksubramanian@appenxus.com>
        Date:   Fri Sep 27 12:43:04 2019 -0400
    
            fixed type
    
        commit a8b6107ce23bbafd9c4171d1b759ed57d7044ba7
        Author: ksubramanian <ksubramanian@appenxus.com>
        Date:   Wed Sep 25 16:32:59 2019 -0400
    
            Updated to AndroidX
    
        commit 3ae03cd8e583a0edb821010beb500ba47ea8e7e0
        Author: ksubramanian <ksubramanian@appenxus.com>
        Date:   Wed Sep 25 16:07:26 2019 -0400
    
            Update to Sample App
    
    commit a0f646bc12d811862d77e914ec66c99452fc7ab7
    Author: Abhas Vohra <avohra@appnexus.com>
    Date:   Thu Sep 12 20:20:43 2019 +0530
    
        Added README for KotlinDemo app, Refactored Directory Structure for Sample apps
    
    commit c7497205dc23547ee6234fb6c7eeb3fd290be424
    Author: Abhas Vohra <avohra@appnexus.com>
    Date:   Thu Jul 25 17:01:54 2019 +0530
    
        Kotlin Sample App

---
+++ examples/java/SimpleVideo/app/src/main/java/com/appnexus/example/simplevideo/MainActivity.java
@@ -0,0 +1,218 @@
+/*
+ *    Copyright 2017 APPNEXUS INC
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.appnexus.example.simplevideo;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import com.appnexus.opensdk.ResultCode;
+import com.appnexus.opensdk.SDKSettings;
+import com.appnexus.opensdk.instreamvideo.ResultCallback;
+import com.appnexus.opensdk.instreamvideo.Quartile;
+import com.appnexus.opensdk.instreamvideo.VideoAd;
+import com.appnexus.opensdk.instreamvideo.VideoAdLoadListener;
+import com.appnexus.opensdk.instreamvideo.VideoAdPlaybackListener;
+
+
+public class MainActivity extends Activity {
+
+    public static final String TAG = MainActivity.class.getName();
+    private TextView infoText;
+    private ImageButton playButon;
+    private ViewGroup.LayoutParams oldParams;
+
+    // The Ad Video instance.
+    // Its important to create this as a Instance variable to make sure its not removed Garbage Collected.
+    private VideoAd videoAd;
+
+    // Content video player.
+    private static VideoView videoPlayer;
+
+    // Ad Container.
+    private RelativeLayout baseContainer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SDKSettings.useHttps(true);
+        infoText = (TextView) findViewById(R.id.infotTextView);
+        playButon = (ImageButton) findViewById(R.id.play_button);
+        videoPlayer = (VideoView) findViewById(R.id.video_player);
+        baseContainer = (RelativeLayout) findViewById(R.id.container_layout);
+        videoPlayer.setVideoURI(Uri.parse(getString(R.string.content_url_1)));
+
+        // Initialize VideoAd
+        videoAd = new VideoAd(this, "17058950");
+
+        // Set the Ad-Load Listener
+        videoAd.setAdLoadListener(new VideoAdLoadListener() {
+            @Override
+            public void onAdLoaded(VideoAd videoAd) {
+                Log.d(TAG, "onAdLoaded");
+                Toast.makeText(getApplicationContext(), "onAdLoaded", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdRequestFailed(VideoAd videoAd, ResultCode errorCode) {
+                Log.d(TAG, "onAdRequestFailed::" + errorCode);
+                Toast.makeText(getApplicationContext(), "onAdRequestFailed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Load the Ad.
+        videoAd.loadAd();
+
+
+        // Set PlayBack Listener.
+        videoAd.setVideoPlaybackListener(new VideoAdPlaybackListener() {
+
+            @Override
+            public void onAdPlaying(final VideoAd videoAd) {
+                Log.d(TAG, "onAdPlaying::");
+            }
+
+            @Override
+            public void onQuartile(VideoAd view, Quartile quartile) {
+                Log.d(TAG, "onQuartile::" + quartile);
+            }
+
+            @Override
+            public void onAdCompleted(VideoAd view, PlaybackCompletionState playbackState) {
+                Log.d(TAG, "onAdCompleted::playbackState" + playbackState);
+                videoPlayer.start();
+            }
+
+            @Override
+            public void onAdMuted(VideoAd view, boolean isMute) {
+                Log.d(TAG, "isAudioMute::" + isMute);
+            }
+
+            @Override
+            public void onAdClicked(VideoAd adView) {
+                Log.d(TAG, "onAdClicked");
+            }
+
+            @Override
+            public void onAdClicked(VideoAd videoAd, String clickUrl) {
+                Log.d(TAG, "onAdClicked");
+            }
+        });
+
+
+        playButon.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (videoAd.isReady()) {
+                    // Play the VideoAd by passing the container.
+                    videoAd.playAd(baseContainer);
+
+                    // Continue to main content video if the ad doesnot start in 2 seconds.
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Log.d(TAG, "getAdPlayElapsedTime");
+                            videoAd.getAdPlayElapsedTime(new ResultCallback() {
+                                @Override
+                                public void onResult(int result) {
+                                    Log.d(TAG, "getAdPlayElapsedTime::onResult::" + result);
+                                    if( result <=0){
+                                        videoPlayer.start();
+                                    }
+                                }
+                            });
+                        }
+                    }, 2000);
+                } else {
+                    videoPlayer.start();
+                }
+                playButon.setVisibility(View.GONE);
+            }
+        });
+
+    }
+
+
+    // Pass the Activity LifeCycle Callback's to VideoAd. This is very important for autoresuming the Video Ad after interruption.
+    @Override
+    public void onResume() {
+        super.onResume();
+        videoAd.activityOnResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        videoAd.activityOnPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        videoAd.activityOnDestroy();
+    }
+
+
+    // Handling Configuration Change.
+    // You just need to resize the container for resizing the video ad for configuration change event. The VideoAd resizes by itself to fit the container.
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            goFullScreen();
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            goBacktoNormal();
+        }
+    }
+
+    public void goFullScreen() {
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int height = displaymetrics.heightPixels;
+        int width = displaymetrics.widthPixels;
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        infoText.setVisibility(View.GONE);
+        RelativeLayout.LayoutParams paramsLandscape = new RelativeLayout.LayoutParams(width, height);
+        oldParams = baseContainer.getLayoutParams();
+        baseContainer.setLayoutParams(paramsLandscape);
+    }
+
+
+    public void goBacktoNormal() {
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        infoText.setVisibility(View.VISIBLE);
+        if (oldParams != null) {
+            baseContainer.setLayoutParams(oldParams);
+        }
+    }
+}
---
+++ examples/kotlin/SimpleDemo/app/src/main/java/appnexus/example/kotlinsample/InterstitialActivity.kt
@@ -0,0 +1,62 @@
+package appnexus.example.kotlinsample
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.appnexus.opensdk.*
+
+class InterstitialActivity : AppCompatActivity(), AdListener {
+
+    private lateinit var interstitial: InterstitialAdView
+
+    private fun showAd() {
+        interstitial.show()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_banner)
+        interstitial = InterstitialAdView(this)
+        interstitial.placementID = "17058950"
+        interstitial.adListener = this
+        interstitial.loadAd()
+    }
+
+    override fun onAdLoaded(ad: AdView?) {
+        log("Interstitial Ad Loaded")
+        showAd()
+    }
+
+    override fun onAdLoaded(nativeAdResponse: NativeAdResponse?) {
+        log("Native Ad Loaded")
+    }
+
+    override fun onAdClicked(adView: AdView?) {
+        log("Ad Clicked")
+    }
+
+    override fun onAdClicked(adView: AdView?, clickUrl: String?) {
+        log("Ad Clicked with URL: $clickUrl")
+    }
+
+    override fun onAdExpanded(adView: AdView?) {
+        log("Ad Expanded")
+    }
+
+    override fun onAdCollapsed(adView: AdView?) {
+        log("Ad Collapsed")
+        finish()
+    }
+
+    override fun onAdRequestFailed(adView: AdView?, resultCode: ResultCode?) {
+        log("Ad Failed: " + resultCode?.name)
+    }
+
+    private fun log(msg: String){
+        Log.d("InterstitialActivity",msg)
+        Toast.makeText(this.applicationContext, msg, Toast.LENGTH_LONG).show()
+    }
+
+
+}
