d3ad7fe94b04ca1969c2e7cf7748ce39c5357567
Abhas Vohra <avohra@appnexus.com>
2021-08-05 15:42:00 +0000

    CASP-6908 MS-4823 Fixed: unnecessary initialization of ANBackgroundThread
    Squashed commit of the following:
    
    commit 4f46099c76b35765f7452f1297e9fee53e69f87c
    Author: Abhas Vohra <avohra@appnexus.com>
    Date:   Wed Aug 4 23:02:57 2021 +0530
    
        Added MAR Scale test
    
    commit 1542f4c8306ed63b31e4dedf5c477e90365d8074
    Author: Abhas Vohra <avohra@appnexus.com>
    Date:   Wed Aug 4 20:11:07 2021 +0530
    
        Added UI test for Banner scale test
    
    commit 57342ce1463f04a02ef2d3c00c9f1de6d3a60047
    Author: Abhas Vohra <avohra@appnexus.com>
    Date:   Fri Jul 30 20:08:28 2021 +0530
    
        WIP
    
    commit 87bb43b0c073e80f725f755609bbdcd858b8259a
    Author: Abhas Vohra <avohra@appnexus.com>
    Date:   Tue Jul 27 15:48:02 2021 +0530
    
        Added null checks for handler
    
    commit a24f2cd3ca9d20e8f59379a3f272ce9d7eae1a0f
    Author: Abhas Vohra <avohra@appnexus.com>
    Date:   Tue Jul 27 02:16:27 2021 +0530
    
        Code cleaning
    
    commit c9312b5e848b40f42a4d5ca8b210004f73e6a559
    Author: Abhas Vohra <avohra@appnexus.com>
    Date:   Thu Jul 1 19:35:55 2021 +0530
    
        Code cleaning
    
    commit b5507ce0d2ea16d56bcecfb9ccf45df97566271d
    Author: Abhas Vohra <avohra@appnexus.com>
    Date:   Wed Jun 23 19:32:43 2021 +0530
    
        Added fix for initialising HandlerThread, and NPE for AdViewRequestManager

---
+++ tests/AppNexusSDKTestApp/app/src/androidTest/java/appnexus/com/appnexussdktestapp/functional/banner/BannerScaleTest.kt
@@ -0,0 +1,102 @@
+/*
+ *    Copyright 2012 APPNEXUS INC
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package appnexus.com.appnexussdktestapp.functional.banner
+
+import android.content.Intent
+import android.content.res.Resources
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.IdlingPolicies
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.runner.AndroidJUnit4
+import appnexus.com.appnexussdktestapp.BannerScaleLoadAndDisplayActivity
+import appnexus.com.appnexussdktestapp.R
+import com.appnexus.opensdk.utils.Clog
+import com.microsoft.appcenter.espresso.Factory
+import kotlinx.android.synthetic.main.activity_mar_load.*
+import org.junit.*
+
+import org.junit.runner.RunWith
+import java.util.concurrent.TimeUnit
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class BannerScaleTest {
+    val Int.dp: Int
+        get() = (this / Resources.getSystem().displayMetrics.density).toInt()
+    val Int.px: Int
+        get() = (this * Resources.getSystem().displayMetrics.density).toInt()
+
+    @get:Rule
+    var reportHelper = Factory.getReportHelper()
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = IntentsTestRule(BannerScaleLoadAndDisplayActivity::class.java, false, false)
+
+    lateinit var bannerActivity: BannerScaleLoadAndDisplayActivity
+
+    @Before
+    fun setup() {
+        IdlingPolicies.setMasterPolicyTimeout(1, TimeUnit.MINUTES)
+        IdlingPolicies.setIdlingResourceTimeout(1, TimeUnit.MINUTES)
+    }
+
+    @After
+    fun destroy() {
+        IdlingRegistry.getInstance().unregister(bannerActivity.idlingResource)
+        reportHelper.label("Stopping App")
+    }
+
+    /*
+    * Test for the Invalid Renderer Url for Banner Native Ad (NativeAssemblyRenderer)
+    * */
+    @Test
+    fun bannerScaleForCountNumberOfBanners() {
+
+        var intent = Intent()
+        var count = 64
+        intent.putExtra(BannerScaleLoadAndDisplayActivity.COUNT, count)
+        mActivityTestRule.launchActivity(intent)
+        bannerActivity = mActivityTestRule.activity
+        IdlingRegistry.getInstance().register(bannerActivity.idlingResource)
+
+        Thread.sleep(2000)
+
+        Espresso.onView(ViewMatchers.withId(R.id.recyclerListAdView))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        Clog.e("COUNT: ", bannerActivity.recyclerListAdView.adapter!!.itemCount.toString())
+
+        (0 until count-1 step 3).forEach { i-> bannerActivity.recyclerListAdView.smoothScrollToPosition(i)
+        Thread.sleep(1000)}
+
+        (count-1 until 0 step 3).forEach { i-> bannerActivity.recyclerListAdView.smoothScrollToPosition(i)
+            Thread.sleep(500)}
+
+        Espresso.onView(ViewMatchers.withId(R.id.recyclerListAdView))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+
+    }
+}
---
+++ tests/AppNexusSDKTestApp/app/src/main/java/appnexus/com/appnexussdktestapp/BannerScaleLoadAndDisplayActivity.kt
@@ -0,0 +1,215 @@
+package appnexus.com.appnexussdktestapp
+
+/*
+ *    Copyright 2014 APPNEXUS INC
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+import android.app.Activity
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.webkit.WebView
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.test.espresso.idling.CountingIdlingResource
+import appnexus.com.appnexussdktestapp.adapter.AdViewRecyclerAdapter
+import appnexus.com.appnexussdktestapp.utility.Utils
+import com.appnexus.opensdk.*
+import com.appnexus.opensdk.utils.Clog
+import kotlinx.android.synthetic.main.activity_mar_load.*
+
+class BannerScaleLoadAndDisplayActivity : Activity() {
+    companion object {
+        val DISPLAY_AD = "DISPLAY_AD"
+        val COUNT = "count"
+    }
+
+    private lateinit var layoutManager: LinearLayoutManager
+    var onLazyLoadAdLoaded: Boolean = false
+    var onLazyAdLoaded: Boolean = false
+    private var displayAd = true
+    var shouldDisplayNativeAd = true
+    private val arrayListAd = ArrayList<Any?>()
+    private val arrayListAdUnits = ArrayList<Ad>()
+    internal var msg = ""
+    lateinit var layout: LinearLayout
+    internal var placementID = "17058950"
+
+    internal var bannerCreativeId = 182424585 //166843311
+
+    internal var idlingResource = CountingIdlingResource("MAR Load Count", true)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mar_load)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true)
+        }
+        SDKSettings.useHttps(true)
+
+        layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerListAdView.layoutManager = layoutManager
+        recyclerListAdView.adapter = AdViewRecyclerAdapter(arrayListAd, this)
+
+        val count = intent.getIntExtra(COUNT, -1)
+        displayAd = intent.getBooleanExtra(DISPLAY_AD, true)
+        Clog.e("COUNT", count.toString())
+        for (i in 0..count) {
+//            idlingResource.increment()
+            arrayListAdUnits.add(setupBannerAd("RTB"))
+        }
+        load()
+    }
+
+    internal fun load() {
+        arrayListAdUnits.forEach({
+            it.loadAd()
+        })
+        Clog.e("MAR TESTING", "IDLING RESOURCE IDLE: " + idlingResource.isIdleNow)
+        if (idlingResource.isIdleNow) {
+            idlingResource.increment()
+        }
+    }
+
+    private fun setupBannerAd(bidType: String, lazyLoad: Boolean = false): BannerAdView {
+        val bav = BannerAdView(this)
+
+        // This is your AppNexus placement ID.
+        bav.placementID = placementID
+        val util = Utils()
+        util.setForceCreativeId(bannerCreativeId, banner = bav)
+
+        bav.allowNativeDemand = false
+        bav.allowVideoDemand = false
+        bav.autoRefreshInterval = 0
+        if (lazyLoad) {
+            bav.enableLazyLoad()
+        }
+        // Turning this on so we always get an ad during testing.
+        bav.shouldServePSAs = false
+
+        // By default ad clicks open in an in-app WebView.
+        bav.clickThroughAction = ANClickThroughAction.OPEN_SDK_BROWSER
+
+        // Get a 300x50 ad.
+        val adSizes = ArrayList<AdSize>()
+        adSizes.add(AdSize(320, 50))
+        bav.adSizes = adSizes
+
+        bav.adListener = object : AdListener {
+            override fun onAdRequestFailed(bav: AdView, errorCode: ResultCode?) {
+                if (errorCode == null) {
+                    Clog.e("SimpleSRM", "Call to loadAd failed")
+                } else {
+                    Clog.e("SimpleSRM", "Ad request failed: $errorCode")
+                    msg += "Banner Ad Failed: $errorCode\n"
+                    toast()
+                }
+                if (!idlingResource.isIdleNow()) {
+                    idlingResource.decrement()
+                }
+            }
+
+            override fun onLazyAdLoaded(adView: AdView?) {
+                msg = "Banner Ad Lazy Loaded\n"
+                toast()
+                onLazyAdLoaded = true
+                arrayListAd.add(adView)
+                if (displayAd)
+                    recyclerListAdView.adapter!!.notifyDataSetChanged()
+                if (!idlingResource.isIdleNow)
+                    idlingResource.decrement()
+            }
+
+            override fun onAdLoaded(av: AdView) {
+                Clog.e("SimpleSRM", "The Ad Loaded!")
+                //                if (av.getParent() != null && av.getParent() instanceof ViewGroup) {
+                //                    ((ViewGroup) av.getParent()).removeAllViews();
+                //                }
+                msg += "Banner Ad Loaded\n"
+                toast()
+                if (!bav.isLazyLoadEnabled()) {
+                    arrayListAd.add(av)
+                } else {
+                    onLazyLoadAdLoaded = true
+                }
+                if (displayAd)
+                    recyclerListAdView.adapter!!.notifyDataSetChanged()
+                if (!idlingResource.isIdleNow)
+                    idlingResource.decrement()
+            }
+
+            override fun onAdLoaded(nativeAdResponse: NativeAdResponse) {
+                Clog.v("SimpleSRM", "Ad onAdLoaded NativeAdResponse")
+                msg += "Banner-Native Ad Loaded\n"
+                toast()
+                arrayListAd.add(nativeAdResponse)
+                if (displayAd)
+                    recyclerListAdView.adapter!!.notifyDataSetChanged()
+                if (!idlingResource.isIdleNow)
+                    idlingResource.decrement()
+            }
+
+            override fun onAdExpanded(bav: AdView) {
+                Clog.v("SimpleSRM", "Ad expanded")
+            }
+
+            override fun onAdCollapsed(bav: AdView) {
+                Clog.v("SimpleSRM", "Ad collapsed")
+            }
+
+            override fun onAdClicked(bav: AdView) {
+                Clog.v("SimpleSRM", "Ad clicked; opening browser")
+            }
+
+            override fun onAdClicked(adView: AdView, clickUrl: String) {
+                Clog.v("SimpleSRM", "onAdClicked with click URL")
+            }
+        }
+
+        return bav
+
+    }
+
+    fun refreshForVisibility() {
+        Handler(Looper.getMainLooper()).post({
+            (recyclerListAdView.adapter as AdViewRecyclerAdapter).setShouldDisplay(
+                shouldDisplayNativeAd
+            )
+        })
+    }
+
+    private fun toast() {
+        if (displayAd) {
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        }
+        Clog.e("TOAST", msg)
+    }
+
+    override fun onDestroy() {
+        if (arrayListAdUnits != null) {
+            arrayListAdUnits.forEach {
+                if (it != null) {
+                    when (it) {
+                        is BannerAdView -> it.activityOnDestroy()
+                    }
+                }
+            }
+        }
+        super.onDestroy()
+    }
+}
