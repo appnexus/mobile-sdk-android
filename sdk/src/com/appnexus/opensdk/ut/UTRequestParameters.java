f2767d8f3c244fbd41bc123e2955dac41c98483b
Kowshickkarthick Subramanian <kowshickkarthick.subramanian@xandr.com>
2022-05-11 13:57:51 +0000

    Merge pull request #628 in MOBILE-SDK/app_mobile-sdk-android from MS-4793_AD_ID to develop
    Squashed commit of the following:
    
    commit ac9aa1d5fd255786ff348d63b3ab5cc4af99b75a
    Author: ksubramanian <ksubramanian@appenxus.com>
    Date:   Tue May 3 12:08:22 2022 -0400
    
        Check for all zero string
    
    commit 2ab552efe69f5fd75d20b0db837aa95cb90904a6
    Author: ksubramanian <ksubramanian@appenxus.com>
    Date:   Mon May 2 16:43:43 2022 -0400
    
        Added AD_ID permission

--- sdk/src/com/appnexus/opensdk/ut/UTRequestParameters.java
+++ sdk/src/com/appnexus/opensdk/ut/UTRequestParameters.java
@@ -1023,7 +1023,7 @@ public class UTRequestParameters {
             Settings.getSettings().deviceAccessAllowed = ANGDPRSettings.canIAccessDeviceData(context);
 
             if (Settings.getSettings().deviceAccessAllowed && !SDKSettings.isAAIDUsageDisabled() && !Settings.getSettings().doNotTrack) {
-                if (!StringUtil.isEmpty(Settings.getSettings().aaid)) {
+                if (!StringUtil.isEmpty(Settings.getSettings().aaid) && !Settings.getSettings().aaid.equalsIgnoreCase("00000000-0000-0000-0000-000000000000")) {
                     JSONObject device_id = new JSONObject();
                     device_id.put(DEVICE_ID_AAID, Settings.getSettings().aaid);
                     device.put(DEVICE_DEVICE_ID, device_id);
